dc43c4a588501d8d79fefec9613a2ea92af64901
Kiyotaka-Ayanak0ji <aryanansik@gmail.com>
2024-06-30 21:30:39 +0530

    FollowBar

--- components/layouts/FollowBar.tsx
+++ components/layouts/FollowBar.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 const FollowBar = () => {
   return (
-    <div className="px-6 py-4 hidden lg:block">
-        <div className="bg-neutral-800 rounded-xl p-4">
-            <h2 className="text-white text-xl font-semibold">
+    <div className='px-2 py-4 hidden lg:block'>
+        <div className='bg-neutral-800 rounded-xl p-4'>
+            <h2 className='text-white text-xl font-semibold'>
                 Who to follow
             </h2>
+            <div className='flex flex-col gap-5 mt-4'>
+                {/* TODO LIST */}
+            </div>
         </div>
     </div>
   )
